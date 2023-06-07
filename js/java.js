785b40227aebb2824af878e99a21fb9b3452911b
luissantos16 <lmsr.1601@gmail.com>
2023-06-07 13:18:51 -0500

    header con responsive

---
+++ js/java.js
@@ -0,0 +1,8 @@
+//menu bar
+
+function llamar(){
+    let menudespegable=document.getElementById("listaid");
+        menudespegable.classList.toggle("lista-open")
+    let listabar=document.getElementById("lista-bar");
+    listabar.querySelector("lista-bar .listabar").classList.toggle;
+}
